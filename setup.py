764294e62e31955b453b288998f7a84ed6ec5383
patquem <patrick.quemere@cea.fr>
2022-08-19 12:47:47 +0200

    change packages

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ setup(
         "matplotlib",
         "scikit-image",
     ],
-    package_dir={"": "pyVSNR"},
-    packages=find_packages(where='pyVSNR'),
+    packages=['pyVSNR'],
 
     description="VSNR (Variational Stationary Noise Remover) algorithm in "
                 "python",
